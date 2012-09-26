1d7fe4bb9e4456c33ce1e99170b80831e7d98086
Tim Ombasa <tim.ombasa@gmail.com>
2012-09-26 14:49:00 -0700

    Adding a redirect in default Application.index() and attributes to the Model. Rendering first page by adding a form and template.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,25 +1,30 @@
 package controllers;
 
+import models.OldBoy;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Application extends Controller {
 
+    static Form<OldBoy> oldBoyForm = form(OldBoy.class);
+
     public static Result index() {
-        return ok("Hello World");
+        return redirect(routes.Application.oldBoys());
     }
 
+    public static Result oldBoys(){
+        return ok(
+          views.html.index.render(OldBoy.all(), oldBoyForm)
+        );
+    }
 
-    public static Result oldboys(){
+    public static Result newOldBoy(){
         return TODO;
     }
 
-    public static Result newOldboy(){
-        return TODO;
-    }
 
-
-    public static Result deleteOldboy(Long id){
+    public static Result deleteOldBoy(Long id){
         return TODO;
     }
 }
